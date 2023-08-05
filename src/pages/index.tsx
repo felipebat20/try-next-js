c77cae6ac070b15a1ddcb54e3dfacbcb12e14172
felipebat20 <felipebatista24680@gmail.com>
2023-08-04 21:46:50 -0300

    feat: initial configuration pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import type { Metadata } from 'next'
+
+export default function Page() {
+  return (
+    <main className="bg-white h-full w-full">
+      <a href="https://assets.musicle.app/img/logo.svg" download="logo">Teeste</a>
+    </main>
+  )
+}
